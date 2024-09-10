4b580de86ceaaef2fdf0297794b80f2b947d0b60
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-10 19:10:37 -0300

    Aplicativo concluido

--- app/src/main/java/com/pitercapistrano/calculadoradenotas/MainActivity.java
+++ app/src/main/java/com/pitercapistrano/calculadoradenotas/MainActivity.java
@@ -1,7 +1,10 @@
 package com.pitercapistrano.calculadoradenotas;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText n1, n2, n3, n4, numeroFaltas;
+    private Button btCalcular;
+    private TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,41 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        n1 = findViewById(R.id.nota1);
+        n2 = findViewById(R.id.nota2);
+        n3 = findViewById(R.id.nota3);
+        n4 = findViewById(R.id.nota4);
+        numeroFaltas = findViewById(R.id.numeroFaltas);
+        btCalcular = findViewById(R.id.btCalcular);
+        txtResultado = findViewById(R.id.txt_resultado);
+
+        btCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double nota1 = Double.parseDouble(n1.getText().toString());
+                double nota2 = Double.parseDouble(n2.getText().toString());
+                double nota3 = Double.parseDouble(n3.getText().toString());
+                double nota4 = Double.parseDouble(n4.getText().toString());
+                int faltas = Integer.parseInt(numeroFaltas.getText().toString());
+
+                double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+                if (faltas <= 20 && media > 5){
+                    txtResultado.setText("Aluno foi aprovado! \n" + "Média Final: " + media);
+                    txtResultado.setTextColor(getColor(R.color.verde));
+                } else if (faltas > 20) {
+                    txtResultado.setText("Aluno reprovado por faltas \n" + "Média Final: " + media + "\nTotal de faltas: " + faltas);
+                    txtResultado.setTextColor(getColor(R.color.vermelho));
+                } else if (faltas <= 20 && media >=4 && media <= 5) {
+                    txtResultado.setText("Aluno de recuperação \n" + "Média Final: " + media);
+                    txtResultado.setTextColor(getColor(R.color.amarelo));
+                } else {
+                    txtResultado.setText("Aluno foi reprovado por nota \n" + "Média Final: " + media);
+                    txtResultado.setTextColor(getColor(R.color.vermelho));
+                }
+            }
         });
     }
 }
